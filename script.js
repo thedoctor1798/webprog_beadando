b1517aa08f72db2a5cf3dcf16a256ac8b7e6a515
ATiKA0 <gluckati78@gmail.com>
2022-05-03 23:00:58 +0200

    HTML+CSS upgrade
    Elkészült a css az alap html felülethez. A teszt miatt be lett szúrva még pár macska. A megbeszéltek alapján megcsináltam a getTitleByIndex functiont.

--- script.js
+++ script.js
@@ -24,8 +24,29 @@ fetch("https://cataas.com/cat").then(response => {
 
 });
 
-console.log(cats)
+fetch("https://cataas.com/cat").then(response => {
+  response.blob().then(blobResponse => {
+    data = blobResponse;
+    const urlCreator = window.URL || window.webkitURL;
+    document.getElementById('myImage2').src = urlCreator.createObjectURL(data);
+  })
 
-function displayCats(catArray){
+});
+
+console.log(cats)
+displayCats(cats)
+function displayCats(catsArray){
     
+}
+
+function getTitleByIndex(index){
+    switch(index){
+        case "type": return "Fajta: ";
+        case "name": return "Név: ";
+        case "color": return "Szín: ";
+        case "age": return "Kor: ";
+        case "gender": return "Nem: ";
+        case "info": return "Leírás";
+        default: return "Egyéb";
+    }
 }
